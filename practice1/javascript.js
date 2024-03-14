188e264619a2ecb0b24c91713d69f1fd4d6912b5
harshdeep <hs4205170@gmail.com>
2024-03-15 00:56:37 +0530

    commit

---
+++ practice1/javascript.js
@@ -0,0 +1,7 @@
+alert("Good morning!");
+alert("hey  my name is Harshdeep!");
+let x=parseFloat(prompt("Enter the cost of T-shirt"));
+let y=parseFloat(prompt("Enter the cost of socks"));
+let z=parseFloat(prompt("Enter the cost of plates"));
+let total=x+y+z;
+console.log("The total cost is:"+ total);
