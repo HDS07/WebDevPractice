93460aaefb596bb9b5a1049f3b26fb096e09c1ed
harshdeep <hs4205170@gmail.com>
2024-03-16 22:09:11 +0530

    add three no. and show in webpage

--- practice1/javascript.js
+++ practice1/javascript.js
@@ -5,3 +5,5 @@ let y=parseFloat(prompt("Enter the cost of socks"));
 let z=parseFloat(prompt("Enter the cost of plates"));
 let total=x+y+z;
 console.log("The total cost is:"+ total);
+
+document.getElementById("heading").innerHTML="The total is : "+total; 
