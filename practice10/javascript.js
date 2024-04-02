2797d79c7c4c94b5bf262660499d46402b0fda8c
harshdeep <hs4205170@gmail.com>
2024-04-02 19:16:04 +0530

    updated RockPaperScissor

--- practice10/javascript.js
+++ practice10/javascript.js
@@ -1,12 +1,25 @@
-function fun(){
+function fun1(){
     let compmove=Math.random();
+    let compchoose='';
     if(compmove>=0&&compmove<1/3){
-        alert( "Computer chose Rock");
+        compchoose='rock';
     }
     else if(compmove>=1/3&&compmove<2/3){
-        alert( "Computer chose scissors");
+        compchoose='scissors';
     }
     else if(compmove>=2/3&&compmove<1){
-        alert( "Computer chose paper");
+        compchoose='paper';
     }
+    
+    let result='';
+    if(compchoose==='rock'){
+        result='Tie.';
+    }
+    else if(compchoose==='scissors'){
+        result='You win.';
+    }
+    else if(compchoose==='paper'){
+        result='You lose';
+    }
+    alert(`You choose rock.Computer choose ${compchoose}.${result}`);
 }
