e63bf753baef8a4a253c51eafc83cc06a8b37e1e
harshdeep <hs4205170@gmail.com>
2024-04-05 11:42:36 +0530

    Smallest word

--- practice4/javascript.js
+++ practice4/javascript.js
@@ -1,13 +1,13 @@
-function longword(str){
+function shortword(str){
     let newstr=str.split(" ");
     newstr.sort(function(a,b){
-        return b.length-a.length;
+        return a.length-b.length;
     })
     return newstr[0];
 }
 function random(){
     let x=document.getElementById("string").value;
-    let y=longword(x);
+    let y=shortword(x);
     document.getElementById( "result" ).innerHTML = y;
     console.log(x);
 }
