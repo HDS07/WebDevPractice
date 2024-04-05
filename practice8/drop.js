43d7d669056393470bb36e69ee9dafe31c435dac
harshdeep <hs4205170@gmail.com>
2024-04-05 23:04:45 +0530

    dropdown

--- practice8/drop.js
+++ practice8/drop.js
@@ -1,7 +1,7 @@
 let ageEL=document.getElementById("age-el")
 
 function ageoption(){
-    for(let i=1 ; i<100 ; i++){
+    for(let i=1 ; i<=10 ; i++){
         ageEL.innerHTML+="<option>"+i+"<option>"
     }
 }
